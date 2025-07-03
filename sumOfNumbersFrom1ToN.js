32adc2ace12ab0ee524acf187757f9a43c50f0a2
s-o-m-i <skullhunter030@gmail.com>
2025-07-03 10:30:03 +0500

    solved a question with while loop

--- sumOfNumbersFrom1ToN.js
+++ sumOfNumbersFrom1ToN.js
@@ -1,10 +1,22 @@
-let n = 1;
+let n = 10;
 let sum = 0;
+let count = 1;
 
-for (let count = 1; count <= n; count++) {
+// for (let count = 1; count <= n; count++) {
+//     sum += count;
+//     console.log(count)
+//     console.log(sum)
+// }
+
+while(count <= n){
     sum += count;
-    console.log(count)
-    console.log(sum)
+    count++
 }
 
 console.log(`Sum of numbers from 1 to ${n} = ${sum} `)
+
+
+
+
+
+
