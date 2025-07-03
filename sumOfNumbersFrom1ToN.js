8debb100e61e55e3f8ee51469523105418b0d468
s-o-m-i <skullhunter030@gmail.com>
2025-07-03 10:22:47 +0500

    solution of sum of numbers from 1 to n

---
+++ sumOfNumbersFrom1ToN.js
@@ -0,0 +1,10 @@
+let n = 1;
+let sum = 0;
+
+for (let count = 1; count <= n; count++) {
+    sum += count;
+    console.log(count)
+    console.log(sum)
+}
+
+console.log(`Sum of numbers from 1 to ${n} = ${sum} `)
